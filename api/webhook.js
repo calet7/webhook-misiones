4c0cfd0b3c68775d79bbacba00d4934052f2b0cb
Calet <calet7@gmail.com>
2026-08-07 18:16:35 -0500

    Fix: forzar Content-Type a text/plain en validacion GET de Meta y enviar challenge crudo para evitar desbordamiento numérico

--- api/webhook.js
+++ api/webhook.js
@@ -11,6 +11,7 @@ module.exports = async function handler(req, res) {
         const challenge = req.query['hub.challenge'];
 
         if (mode === 'subscribe' && token === process.env.VERIFY_TOKEN) {
+            res.setHeader('Content-Type', 'text/plain');
             return res.status(200).send(challenge);
         }
         return res.status(403).send('Forbidden');
