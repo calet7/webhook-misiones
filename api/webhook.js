28a6232dc9932251eefcaac8ea7c6c7cf640c540
Calet <calet7@gmail.com>
2026-08-07 15:22:20 -0500

    Refactor: separar endpoints en api/webhook.js y api/cron-serie.js

--- api/webhook.js
+++ api/webhook.js
@@ -229,6 +229,21 @@ async function handleTriageAction(user, actionId, senderPhone, whatsappPhoneNumb
     }
 }
 
+app.get('/', (req, res) => {
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
+
+    if (mode && token) {
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+            return res.status(200).send(challenge);
+        }
+        return res.sendStatus(403);
+    }
+
+    return res.sendStatus(400);
+});
+
 app.post('/', async (req, res) => {
     try {
         if (!verifyMetaSignature(req)) {
